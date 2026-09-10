1fc4a5f34af206c6bddc7e14115397c149813a7b
Pintu Saini <pintusaininch2979@gmail.com>
2026-09-10 11:47:27 +0530

    fetch model from github only

--- app/config.py
+++ app/config.py
@@ -15,7 +15,7 @@ class PATH:
     CHECKPOINT_DIR: Path = BASE_DIR / "checkpoints"
     DOWNLOAD_MODEL_PATH: Path = BASE_DIR / "model"
     # DATASET_DIR: Path = BASE_DIR / "dataset"
-    CHECKPOINT_PATH_FOR_LOAD: Path = CHECKPOINT_DIR / "best_val_acc_model_2.pth"
+    CHECKPOINT_PATH_FOR_LOAD: Path = CHECKPOINT_DIR / "resnet18_food_deploy.pth"
     CHECKPOINT_PATH_FOR_SAVE: Path = CHECKPOINT_DIR / "resnet18_food_deploy.pth"
     
     
--- app/model_loader.py
+++ app/model_loader.py
@@ -5,14 +5,14 @@ import sys, os, torch
 from app.config import PATH, IMAGE
 
 #=====================================================
-from huggingface_hub import hf_hub_download
+# from huggingface_hub import hf_hub_download
 #=====================================================
 #THIS IS NEEDED FOR RENDER DEPLOYMENT
 #=====================================================
-best_model_load_path = hf_hub_download(
-    repo_id="pintusaini2979/resnet18-food-classifier",
-    filename="resnet18_food_deploy.pth"
-)
+# best_model_load_path = hf_hub_download(
+#     repo_id="pintusaini2979/resnet18-food-classifier",
+#     filename="resnet18_food_deploy.pth"
+# )
 #=====================================================
 
 # torch.hub.set_dir(PATH.DOWNLOAD_MODEL_PATH)
@@ -23,10 +23,10 @@ model.eval()
 #=====================================================
 #THIS IS NEEDED FOR LOCAL INFERENCE
 #=====================================================
-# best_model_load_path = PATH.CHECKPOINT_PATH_FOR_LOAD
-# if not os.path.exists(best_model_load_path):
-#     myLog("No checkpoint found. Can't predict.")
-#     sys.exit(1)
+best_model_load_path = PATH.CHECKPOINT_PATH_FOR_LOAD
+if not os.path.exists(best_model_load_path):
+    myLog("No checkpoint found. Can't predict.")
+    sys.exit(1)
 #=====================================================
 
 myLog("Checkpoint found. Using Model for Inference")
